43811095e46e273d6142ca2e083451afd9d960fe
Stanislav <s.chihichin737@gmail.com>
2022-11-06 18:34:41 +0300

    created server app

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+app.listen(process.env.PORT, () => {
+  console.log('Server listening on port ' + process.env.PORT);
+});
+
+app.get('/', (req, res) => {
+  const washing = [
+    {
+      id: 1,
+      name: 'washing 1',
+      price: 80,
+      talons: [
+        {
+          time: '13:40',
+        },
+        {
+          time: '14:40',
+        },
+        {
+          time: '15:40',
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: 'washing 2',
+      price: 300,
+      talons: [
+        {
+          time: '10:14',
+        },
+        {
+          time: '12:30',
+        },
+      ],
+    },
+  ];
+  res.json(washing);
+});
