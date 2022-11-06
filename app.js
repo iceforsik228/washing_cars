89f1ac76c848cf4e43ae2b7e38a32cf84fc960ca
Stanislav <s.chihichin737@gmail.com>
2022-11-06 19:38:51 +0300

    made some fixes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
 const washingSchema = require('./models/washing');
@@ -12,6 +13,8 @@ app.listen(process.env.PORT, (error) => {
 });
 
 const Washing = mongoose.model('Washing', washingSchema);
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   const washing = [
