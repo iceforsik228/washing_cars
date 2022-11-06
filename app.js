dafe1b5f6abb4a303ebee16d17a84fe38f814b9b
Stanislav <s.chihichin737@gmail.com>
2022-11-06 20:31:30 +0300

    made some fixes

--- app.js
+++ app.js
@@ -1,32 +1,46 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 require('dotenv').config();
 
 const washingSchema = require('./models/washing');
 
-mongoose.connect(process.env.MONGO_URI);
 const app = express();
 
+app.use(morgan('dev'));
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then((res) => console.log('Connected to DB'))
+  .catch((error) => console.log(error));
+
 app.listen(process.env.PORT, (error) => {
-  error ? console.log(errorMsg(error)) : console.log(`listening port ${process.env.PORT}`);
+  error ? console.log(error) : console.log(`listening port ${process.env.PORT}`);
 });
 
 const Washing = mongoose.model('Washing', washingSchema);
 
-app.get('/', (req, res) => {
+app.get('/api/washing', (req, res) => {
   res.header({ 'Access-Control-Allow-Origin': '*' });
   Washing.find().then((washing) => res.status(200).json(washing));
 });
 
-app.get('/add-washing', (req, res) => {
-  const washing = new Washing({
-    name: 'washing',
-    description: 'qweqwe',
-    distance: 123,
-    talons: [1, 2, 3],
-  });
-  washing.save((error, data) => {
-    if (error) console.log(error);
-  });
-  res.json({ message: 'success' });
+app.get('/api/washing/:id', (req, res) => {
+  res.header({ 'Access-Control-Allow-Origin': '*' });
+  Washing.findById(req.params.id).then((washing) => res.status(200).json(washing));
+});
+
+app.post('/api/add-washing', (req, res) => {
+  const { name, description, distance, talons } = req.body;
+  const washing = new Washing({ name, description, distance, talons });
+  washing
+    .save()
+    .then((wash) => res.status(200).json({ message: 'success' }))
+    .catch((error) => {
+      console.log(error);
+    });
 });
