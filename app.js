73176a63f3f538030a457864108c24fa9a5409cb
Stanislav <s.chihichin737@gmail.com>
2022-11-17 18:52:31 +0300

    made some fixes

--- app.js
+++ app.js
@@ -31,24 +31,7 @@ app.get('/api/washing', (req, res) => {
   });
 });
 
-app.get('/api/washing/:idWashing/:idTime', (req, res) => {
-  Washing.findById(req.params.idWashing, (err, washing) => {
-    if (err) return res.status(500).send(err);
-    res.status(200).json(washing['talons'][req.params.idTime]);
-  });
-});
-
-app.get('/api/washing/:id', (req, res) => {
-  Washing.findById(req.params.id)
-    .then((washing) => {
-      res.status(200).json(washing);
-    })
-    .catch((err) => {
-      res.status(500).send(err.message);
-    });
-});
-
-app.post('/api/add-washing', (req, res) => {
+app.post('/api/washing', (req, res) => {
   const { name, description, distance, talons } = req.body;
   const washing = new Washing({ name, description, distance, talons });
   washing
@@ -57,4 +40,59 @@ app.post('/api/add-washing', (req, res) => {
     .catch((error) => {
       console.log(error);
     });
+});
+
+app.get('/api/washing/:id', (req, res) => {
+  Washing.findById(req.params.id, (err, washing) => {
+    if (err) return res.status(500).send(err);
+    res.status(200).json(washing);
+  });
+});
+
+app.put('/api/washing/:id', (req, res) => {
+  const { boxesCount } = req.body;
+  if (boxesCount < 0 || boxesCount > 100) {
+    res.status(500).json({ err: 'frontend krivoy' });
+  }
+  Washing.findById(req.params.id, (err, washing) => {
+    if (err) return res.status(500).json(err);
+    washing['talons'].forEach((element) => {
+      element['boxesCount'] = boxesCount;
+    });
+    washing.save((err) => {
+      if (err) return res.status(500).json(err);
+      res.status(200).json(washing);
+    });
+  });
+});
+
+app.put('/api/washing/:id/:idTime', (req, res) => {
+  const { boxesCount } = req.body;
+  if (boxesCount < 0 || boxesCount > 100) {
+    res.status(500).json({ err: 'frontend krivoy' });
+  }
+  Washing.findById(req.params.id, (err, washing) => {
+    if (err) return res.status(500).json(err);
+    washing['talons'][req.params.idTime]['boxesCount'] = boxesCount;
+    washing.save((err) => {
+      if (err) return res.status(500).json(err);
+      res.status(200).json(washing);
+    });
+  });
+});
+
+app.put('/api/washing/talon/:id/:idTime', (req, res) => {
+  Washing.findById(req.params.id, (err, washing) => {
+    if (err) return res.status(500).send(err);
+
+    if (washing['talons'][req.params.idTime]['boxesCount'] > 0) {
+      washing['talons'][req.params.idTime]['boxesCount']--;
+    } else {
+      res.status(500).json({ err: 'frontend krivoy' });
+    }
+    washing.save((err) => {
+      if (err) return res.status(500).json(err);
+      res.status(200).json(washing);
+    });
+  });
 });
