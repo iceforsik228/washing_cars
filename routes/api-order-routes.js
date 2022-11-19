19d987bec48c98dfdea1f345398dfcf4617fea63
Stanislav <s.chihichin737@gmail.com>
2022-11-19 22:50:41 +0300

    frontender plohoy chel

--- routes/api-order-routes.js
+++ routes/api-order-routes.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const router = express.Router();
 
-const orderSchema = require('../models/washing');
+const orderSchema = require('../models/order');
 
 const Order = mongoose.model('Order', orderSchema);
 
@@ -17,10 +17,10 @@ router.get('/api/order', (req, res) => {
 router.post('/api/order', (req, res) => {
   const { surname, name, phone, paymentMethod } = req.body;
   const order = new Order({ surname, name, phone, paymentMethod });
-  order.save((err) => {
-    if (err) return res.status(404).json({ message: 'krivoy frontend' });
-    res.status(200).json({ message: 'ya bog' });
-  });
+  order
+    .save()
+    .then(() => res.json({ message: 'backender - bog' }))
+    .catch((err) => res.status(404).json(err));
 });
 
 module.exports = router;
